95910011ddca190a269e08af2f475810598931bc
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-02 03:36:43 +0600

    socket implement to client side

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const http = require('http');
 const expressServer = http.createServer(app)
 const port = 5000 || process.env.PORT;
 
+
+const {Server} = require("socket-io")
+const io = new Server(expressServer)
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
